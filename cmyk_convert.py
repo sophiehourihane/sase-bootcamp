0afbe10ad5549c96ebcd9ad46a5c9b5a1b30278e
how4rd <15080656+how4rd@users.noreply.github.com>
2022-08-16 12:35:49 -0700

    Implement array-based solution

--- cmyk_convert.py
+++ cmyk_convert.py
@@ -20,46 +20,35 @@ def rescale_by_element(original_cmyk_array, ink_limit):
     for row in range(rows):
         for col in range(cols):
             element = cmyk_array[row, col]
-            total_ink = sum(element)
-            if total_ink > ink_limit:
+            total_ink_in_pixel = sum(element)
+            if total_ink_in_pixel > ink_limit:
                 # rescale here
-                amount_over = total_ink - ink_limit
-                percent_of_ink = element / total_ink
-                element -= (amount_over * percent_of_ink).astype(np.uint8)
+                amount_pixel_over = total_ink_in_pixel - ink_limit
+                percent_of_ink = element / total_ink_in_pixel
+                element -= (amount_pixel_over * percent_of_ink).astype(np.uint8)
     return cmyk_array
 
 def rescale_by_array(original_cmyk_array, ink_limit):
     cmyk_array = np.copy(original_cmyk_array)
-    rows = np.shape(cmyk_array[:, 0])[0]
-    #cols = np.shape(cmyk_array[0, :])[0]
 
-    total_ink = np.sum(cmyk_array, axis = 2)
-    print(cmyk_array.shape)
-    print(total_ink.shape)
+    total_ink_in_pixel = np.sum(cmyk_array, axis = 2)
+    amount_pixel_over = total_ink_in_pixel.astype(np.int8) - ink_limit
+    pixel_rescaling_factor = ink_limit / total_ink_in_pixel
+    pixel_rescaling_factor[amount_pixel_over <= 0] = 1
 
-    amount_over = total_ink - ink_limit
-    percent_of_ink = cmk_array / total_ink
-    cmk_array[amount_over > 0] -= (percent_of_ink * )#cmk_array[where] / total_ink)
+    for i in range(4):
+        cmyk_array[:, :, i] = np.floor(cmyk_array[:, :, i] * pixel_rescaling_factor)
 
-    # for row in range(rows):
-    #     for col in range(cols):
-    #         element = cmyk_array[row, col]
-    #         total_ink = sum(element)
-    #         if total_ink > ink_limit:
-    #             # rescale here
-    #             amount_over = total_ink - ink_limit
-    #             percent_of_ink = element / total_ink
-    #             element -= (amount_over * percent_of_ink).astype(np.uint8)
     return cmyk_array
 
-ink_limit = 200 #somewhere between 0 and 400
+ink_limit = 400 #somewhere between 0 and 400
 test_image = 'testimage.tiff'
 save_image = 'saveimage.tiff'
 
 cmyk_array_original = tiff_to_array(test_image)
 cmyk_array = rescale_by_array(cmyk_array_original, ink_limit)
-#cmyk_array = rescale(cmyk_array_original, ink_limit)
-#array_to_tiff(cmyk_array, save_image)
+# cmyk_array = rescale(cmyk_array_original, ink_limit)
+array_to_tiff(cmyk_array, save_image)
 
 
 
